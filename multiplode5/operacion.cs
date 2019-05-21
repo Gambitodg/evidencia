3f561d5bbb7d5311be195a2ca6e93bdaa9cd6adf
Gambitodg <50213808+Gambitodg@users.noreply.github.com>
2019-05-21 01:28:50 -0500

    correccion 1

---
+++ multiplode5/operacion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplode5
+{
+    class operacion
+    {
+    }
+}
